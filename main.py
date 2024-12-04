ade36cb78330226fba97f310350f254c00c4cdca
hsanders-07 <hsander7@byu.edu>
2024-12-04 15:31:03 -0700

    update libraries

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import zipfile
-import plotly.express as px
+#import zipfile
+#import plotly.express as px
 import matplotlib.pyplot as plt
 import requests
-from io import BytesIO
-import plotly.graph_objects as go
+#from io import BytesIO
+#import plotly.graph_objects as go
 #from plotly.subplots import make_subplots
 from my_plots import *
 import streamlit as st
